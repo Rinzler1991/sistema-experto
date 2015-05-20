8efe194fc6644314637d3c79bc7cbf84040f4d2f
Carlo Daniel <wyldepanther@hotmail.com>
2015-05-19 19:31:04 -0700

    Acceso a datos completo

---
+++ Selector/Form3.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Selector
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textUsr = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.btnCerrar.Location = new System.Drawing.Point(12, 396);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(75, 29);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 10F);
+            this.button2.Location = new System.Drawing.Point(361, 396);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Salir del Programa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // textUsr
+            // 
+            this.textUsr.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.textUsr.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textUsr.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textUsr.ForeColor = System.Drawing.Color.White;
+            this.textUsr.Location = new System.Drawing.Point(12, 12);
+            this.textUsr.Name = "textUsr";
+            this.textUsr.Size = new System.Drawing.Size(453, 19);
+            this.textUsr.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtNombre.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.txtNombre.Location = new System.Drawing.Point(12, 37);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(186, 24);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtDesc.Font = new System.Drawing.Font("Times New Roman", 11F);
+            this.txtDesc.Location = new System.Drawing.Point(12, 95);
+            this.txtDesc.Multiline = true;
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(238, 273);
+            this.txtDesc.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Descripción del instrumento:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(256, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(243, 331);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(511, 437);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDesc);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.textUsr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnCerrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resultado";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textUsr;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ Selector/Form3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Selector
+{
+    public partial class Form3 : Form
+    {               
+        MySqlCommand Query = new MySqlCommand();
+        MySqlConnection Conexion;
+        MySqlDataReader consultar;
+        public string sql = "server=localhost; user id=root; password = BIONICLE4012; persistsecurityinfo=True; database=datos";
+
+        
+        
+
+        public Form3()
+        {            
+            InitializeComponent();
+            
+            textUsr.Text =  Form1.usrName+", este es el instrumento que quizás podrías tocar:"; //Texto de bienvenida, cargado al iniciar el formulario.
+
+            try //Intentar consulta.
+            {
+                Conexion = new MySqlConnection();
+                Conexion.ConnectionString = sql;
+                Conexion.Open();
+                Query.CommandText = "SELECT * FROM datos.instrumentos WHERE idinstrumentos="+Form2.idInstrumento;
+                Query.Connection = Conexion;
+                consultar = Query.ExecuteReader();
+
+                while (consultar.Read())
+                {
+                    string nombre = consultar.GetString(1);
+                    string descripcion = consultar.GetString(2);
+
+                    txtNombre.Text = nombre;
+                    txtDesc.Text = descripcion;
+                }
+
+                Conexion.Close();
+            }
+
+            catch (MySqlException ex) //Reacción en caso de error.
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(Form1.usrName+", ¿seguro que quieres salir y cerrar el programa?", "Confirmación de salida", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation) == DialogResult.Yes)
+            {
+                Application.Exit(); // Si el usuario eligió "Sí", cerrar aplicación.
+            }        
+        }
+
+        public void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            
+            this.Close();
+        }
+       
+    }
+}
